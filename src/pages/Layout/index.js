98deb774b36bd06b4e81d570b695fa9a522b7179
赫英博 <heyingbo@heyingbodeMacBook-Air.local>
2024-07-12 14:16:20 +0100

    二级路由的配置

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,12 +1,55 @@
-// 测试token是否成功注入
-import { request } from "@/utils"
-import { useEffect } from "react"
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+    HomeOutlined,
+    DiffOutlined,
+    EditOutlined,
+    LogoutOutlined
+} from '@ant-design/icons'
+import './index.scss'
+import { Outlet } from 'react-router-dom'
 
-const Layout = () => {
-    useEffect(() => {
-        request.get('/user/profile')
-    }, [])
-    return <div>this is layout</div>
+const { Header, Sider } = Layout
+
+const GeekLayout = () => {
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <div className="user-info">
+                    <span className="user-name">user.name</span>
+                    <span className="user-logout">
+                        <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+                            <LogoutOutlined /> 退出
+                        </Popconfirm>
+                    </span>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        theme="dark"
+                        defaultSelectedKeys={['1']}
+                        style={{ height: '100%', borderRight: 0 }}
+                    >
+                        <Menu.Item icon={<HomeOutlined />} key="1">
+                            首页
+                        </Menu.Item>
+                        <Menu.Item icon={<DiffOutlined />} key="2">
+                            文章管理
+                        </Menu.Item>
+                        <Menu.Item icon={<EditOutlined />} key="3">
+                            创建文章
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="layout-content" style={{ padding: 20 }}>
+                    {/* 二级路由出口 */}
+                    <Outlet />
+                </Layout>
+            </Layout>
+        </Layout>
+    )
 }
 
-export default Layout
+export default GeekLayout
