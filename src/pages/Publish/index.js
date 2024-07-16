3116bb6e6186ce01954c0cc7caa0297cf3658ff5
赫英博 <heyingbo@heyingbodeMacBook-Air.local>
2024-07-17 00:34:05 +0100

    发布文章-接入富文本编辑器

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -12,6 +12,8 @@ import {
 import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const { Option } = Select
 
@@ -75,6 +77,11 @@ const Publish = () => {
                         rules={[{ required: true, message: '请输入文章内容' }]}
                     >
                         {/* 富文本编辑器 */}
+                        <ReactQuill
+                            className="publish-quill"
+                            theme="snow"
+                            placeholder="请输入文章内容"
+                        />
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 4 }}>
