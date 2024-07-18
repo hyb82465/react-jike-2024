77348ff4d7689a27d982ccec7ce217aef320179c
赫英博 <heyingbo@heyingbodeMacBook-Air.local>
2024-07-18 13:29:37 +0100

    文章列表-携带id跳转编辑页

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm } from 'antd'
 // 时间选择器汉化包
 import locale from 'antd/es/date-picker/locale/zh_CN'
@@ -15,6 +15,7 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+    const navigate = useNavigate()
     const { channelList } = useChannel()
     // 定义状态枚举
     const status = {
@@ -65,7 +66,7 @@ const Article = () => {
             render: data => {
                 return (
                     <Space size="middle">
-                        <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                        <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={() => navigate(`/publish?id=${data.id}`)} />
                         <Popconfirm
                             title="删除文章"
                             description="确认要删除当前文章吗?"
